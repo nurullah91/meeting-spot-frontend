84ddbb3660e03e4719217c8f1a4094b7d14e83d0
MD NURULLAH BHUIYAN <121657120+nurullah91@users.noreply.github.com>
2025-02-08 01:24:24 +0600

    related product section designed and responsive done

--- src/pages/Rooms/RelatedRooms.tsx
+++ src/pages/Rooms/RelatedRooms.tsx
@@ -1,5 +1,10 @@
-import { Spin } from "antd";
+import { Col, Row, Spin } from "antd";
 import { useGetAllRoomsQuery } from "../../redux/features/user/userAccess.api";
+import CustomContainer from "../../components/CustomContainer";
+import { TRoom } from "../../types/user.types";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../lib/motionVariant";
+import RoomCard from "../../components/ui/RoomCard";
 
 export interface IRelatedRoomsProps {
   category: string;
@@ -32,16 +37,43 @@ export default function RelatedRooms({ category }: IRelatedRoomsProps) {
   }
   return (
     <div>
-      <h1
-        style={{
-          fontWeight: "bold",
-          textAlign: "center",
-          color: "#f43f5e",
-          fontSize: "1.875rem",
-        }}
-      >
-        This is RelatedRooms component
-      </h1>
+      {/* Main Content of the room page */}
+      <CustomContainer>
+        <div>
+          <h2
+            style={{
+              borderBottom: "1px solid #cdcccc",
+              fontSize: "24px",
+              paddingBottom: "8px",
+              margin: "30px 0px",
+            }}
+          >
+            Related rooms
+          </h2>
+
+          <div style={{ margin: "30px 0px", overflow: "hidden" }}>
+            <Row gutter={[15, 15]}>
+              {allRoomsData?.data?.result?.map((room: TRoom, index: number) => (
+                <Col
+                  span={24}
+                  md={{ span: 12 }}
+                  lg={{ span: 6 }}
+                  key={room._id}
+                >
+                  <motion.div
+                    variants={fadeIn(index % 2 === 1 ? "left" : "right", 0)}
+                    initial="hidden"
+                    whileInView={"show"}
+                    viewport={{ once: false, amount: 0.5 }}
+                  >
+                    <RoomCard room={room} />
+                  </motion.div>
+                </Col>
+              ))}
+            </Row>
+          </div>
+        </div>
+      </CustomContainer>
     </div>
   );
 }
